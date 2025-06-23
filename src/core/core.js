68643286a94347e5533002335f7a6f22534c03d7
antelopest <antelopest@gmail.com>
2025-06-23 15:19:07 +0700

    main add core.js

--- src/core/core.js
+++ src/core/core.js
@@ -1,1 +1,5 @@
-import Store from './store/Store';
+import Store from './store/Store.js';
+import Router from './router/Router.js';
+import Service from './service/Service.js';
+
+export default { Store, Router, Service };
--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import core from './core/core';
 import shared from './shared/shared.js';
 
 import SidebarComponent from './shared/sidebar/sidebar.component';
+import Router from './core/router/Router';
 
 document.getElementById('app').innerHTML = SidebarComponent();
