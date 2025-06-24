ba62e5ed69d8c23e0d6c6c6b195275725d418243
antelopest <antelopest@gmail.com>
2025-06-24 14:11:46 +0700

    main add routing app

--- src/index.js
+++ src/index.js
@@ -7,3 +7,25 @@ import SidebarComponent from './shared/components/sidebar/sidebar.component';
 import Router from './core/router/Router';
 
 document.getElementById('app').innerHTML = await SidebarComponent();
+
+/*
+ * public
+ * /login
+ * /signup
+ *
+ * private
+ * /today
+ * /calendar
+ * /events
+ * /tasks
+ * /tags
+ * /help
+ * /settings
+ *
+ *
+ * /filters
+ * all
+ * english
+ * development
+ * sport
+ * */
