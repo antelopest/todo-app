ae6abbca2e23b38777fb01dddf7c867d54fe1340
antelopest <antelopest@gmail.com>
2025-08-23 13:29:34 +0700

    main add html, css

--- src/main.js
+++ src/main.js
@@ -15,10 +15,22 @@ document.addEventListener('DOMContentLoaded', () => {
   document.body.classList.add(theme);
 });
 
+const todos = [];
+
 const form = document.getElementById('todo__form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+});
+
+const toggleThemes = document.getElementById('todo__theme-toggle');
+
+toggleThemes.addEventListener('click', () => {
+  if (document.body.classList.contains(Themes.light)) {
+    document.body.classList.replace(Themes.light, Themes.dark);
+  } else {
+    document.body.classList.replace(Themes.dark, Themes.light);
+  }
 });
 
 const Filters = {
